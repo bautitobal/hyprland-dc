1f7c2a7993e9d6d4c21f5fda80ecac4a9e4cdeec
bautitobal <bautitobal11@gmail.com>
2025-06-29 16:46:35 -0300

    Fix toggle-waybar script

--- scripts/toggle-waybar.sh
+++ scripts/toggle-waybar.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+pgrep -x mako || mako &
+
 if pgrep -x waybar > /dev/null; then
+    dunstify -a "Waybar" "🚫 Waybar disabledó" -t 100
+    sleep 0.2  # Pequeña pausa para asegurar la notificación
     pkill waybar
-    dunstify -a "Waybar" "Se ocultó"
 else
     waybar &
-    dunstify -a "Waybar" "Activada"
+    dunstify -a "Waybar" "✅ Waybar enabled" -i "waybar"
 fi
